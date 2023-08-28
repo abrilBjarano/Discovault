bc8c1faf376614c2883250c7ba593f123b25e551
abrilxcx <abrlbjrn@gmail.com>
2023-08-28 02:40:21 -0600

    paso 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ColeccionDiscosModule } from './coleccion-discos/coleccion-discos.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ColeccionDiscosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/coleccion-discos/pages/main-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'coleccion-discos-main',
+  templateUrl: 'main-page.component.html'
+})
+
+export class MainPageComponent {}
