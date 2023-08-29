bd0e71073761b4939e791444afb43c5220d7a41c
abrilxcx <abrlbjrn@gmail.com>
2023-08-29 12:11:39 -0600

    services

--- src/app/record-collection/pages/main-page.component.ts
+++ src/app/record-collection/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Record } from '../interfaces/record.interface';
+import { RecordCollectionService } from '../services/record-collection.service';
 
 @Component({
   selector: 'coleccion-discos-main',
@@ -7,31 +8,5 @@ import { Record } from '../interfaces/record.interface';
 })
 
 export class MainPageComponent {
-
-  public recordsMain: Record[] = [
-    {
-      name: 'Fijación Oral Vol.1',
-      by: 'Shakira',
-      songs: 12,
-      release_date: 2005
-    },{
-    name: 'Dónde están los ladrones?',
-    by: 'Shakira',
-    songs: 11,
-    release_date: 1998
-  },{
-    name: 'Pies Descalzos',
-    by: 'Shakira',
-    songs: 11,
-    release_date: 1995
-  }]
-
-  onNewRecordMain( record: Record ): void {
-    this.recordsMain.push( record );
-  }
-
-  onDeleteRecordMain( index: number ): void {
-    this.recordsMain.splice(index, 1);
-  }
-
+  constructor( public recordCollectionService: RecordCollectionService ) {}
 }
